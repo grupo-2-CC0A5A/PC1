45eb0784b4fecd5cfb399868c5b6bf25b74ac9b6
Alex Vega <axvg@users.noreply.github.com>
2025-09-19 22:03:27 -0500

    simple test

---
+++ tests/test_item.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.item import Item, ItemDB
+from src.db import DB
+
+
+class TestItem:
+    def test_bar(self):
+        assert True
+
+    def test_foo(self):
+        assert False
